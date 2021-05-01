fd6702793f6b8fe18a89af833db3e43e80b04c1c
kremenevskiy <kremeneuski@gmail.com>
2021-05-01 15:42:39 +0300

    created player could shoot

---
+++ index.js
@@ -0,0 +1,87 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Player {
+    constructor(x, y, radius, color){
+        this.x = x;
+        this.y = y;
+
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+       c.beginPath()
+       c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false); 
+       c.fillStyle = this.color;
+       c.fill();
+    }
+}
+
+
+class Projectile {
+    constructor(x, y, radius, color, velocity){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+           c.beginPath()
+           c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false); 
+           c.fillStyle = this.color;
+           c.fill();
+        }
+
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, 'blue');
+player.draw();
+
+
+const projectiles = [];
+ 
+console.log(player);
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    projectiles.forEach((projectile) => {
+        projectile.update();
+    })
+}
+
+
+window.addEventListener('click', (event) => {
+        const angle = Math.atan2(event.clientY - canvas.height / 2,
+            event.clientX - canvas.width / 2);
+        console.log(angle*180/Math.PI);
+ 
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+        projectiles.push(new Projectile(
+            canvas.width / 2, canvas.height / 2,
+            5, 'red', velocity
+        ))
+        
+        console.log(projectiles.length);
+    })
+
+animate();
