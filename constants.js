6a1ef8ea82e948531d90f49a2a95d2a7eccb5485
kremenevskiy <kremeneuski@gmail.com>
2021-05-04 14:54:38 +0300

    game constants

---
+++ constants.js
@@ -0,0 +1,17 @@
+module.exports = Object.freeze({
+    PLAYER_MAX_HP: 100,
+    PLAYER_RADIUS: 20,
+    PLAYERS_SPEED: 100,
+
+    BULLET_RADIUS: 3,
+    BULLET_SPEED: 300,
+    BULLET_DAMAGE: 10,
+
+    FOOD_RADIUS: 4,
+
+    MAP_SIZE: 3000,
+    MSG_TYPES: {
+        JOIN_GAME: 'join_game',
+        GAME_OVER: 'dead'
+    }
+});
