a00c5ea8d147363753e8b98b189a510d4ded91b0
kremenevskiy <kremeneuski@gmail.com>
2021-05-05 02:44:18 +0300

    divided into modules and functions, added room entry point

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const Player = require('./player')
 var express = require('express');
 const Food = require('./food')
 const Bullet = require('./bullet')
+const Constants = require('./constants')
+const Room = require('./room')
+const Vector = require('./vector')
 
 
 const width = 1000;
@@ -20,8 +23,6 @@ app.use(express.static('public'));
 
 
 var io = require('socket.io')(server);
-var players = [];
-var bullets = [];
 
 
 function getRandomColor() {
@@ -34,113 +35,66 @@ function getRandomColor() {
 }
 
 
-// var N_food = 1000;
-// var eat = [];
-// for(var i = 0; i < N_food; ++i){
-//     eat.push(new Food(i, (Math.round(Math.random()) * 2 - 1) * Math.random() * width,(Math.round(Math.random()) * 2 - 1) *  Math.random() * height, 
-//     getRandomColor()))
-// }
-
-// console.log("lenght eat: " + eat.length)
-
-
-
-
-// class Player {
-//     constructor(id, x, y, r){
-//         this.id = id;
-//         this.x = x;
-//         this.y = y;
-//         this.r = r;
-//     }
-// }
-
-setInterval(heartbeat, 30);
-
-function heartbeat() {
-
-    bullets.forEach((bullet) => {
-        bullet.update();
-    })
-
-
-    io.sockets.emit('heartbeatbullet', bullets);
-
-    // players.forEach((player) => {
-    //     for (var i = 0; i < eat.length; i++){
-    //         if (player.eats(eat[i])){
-    //             eat.splice(i, 1);
-    //         }
-    //     }
-    // })
-
-
-    io.sockets.emit('heartbeat', players);
-    // if (players.length > 0) {
-    //     console.log('sending eat: ')
-    //     eat.forEach((food) => {
-    //         console.log('foody:' + food)
-    //     })
-    // }
-    // io.sockets.emit('heartbeatfood', eat)
-    // console.log("eat: " + eat);
-    // for (var i = 0; i < eat.length; i++){
-    //     console.log(eat[i]);
-    // }
-    // for (var i = 0; i < players.length; i++){
-    //     console.log(players[i]);
-    // }
-    // console.log("sending players: "  + players.length + " " + players)
-}
-
-
 io.on('connection', newConnection);
 function newConnection(socket){
     console.log("New client: " + socket.id);
-    
-    socket.on('start', 
-        function(data) {
-            console.log(socket.id + " x: " + data.x + " y: " + data.y + " r: " + data.r);
-            var newPlayer = new Player(socket.id, data.x, data.y, data.r)
-            players.push(newPlayer);
-        }
-    )
+    socket.on(Constants.MSG_TYPES.JOIN_GAME, joinGame);
+    socket.on(Constants.MSG_TYPES.UPDATE_INPUT, updatePlayer);
+    socket.on('newbullet', addBullet);
+    socket.on('disconnect', onDisconnect);
 
-    // socket.on('updateeat', 
-    //     function(data){
-    //         eat = data;
-    //     }
-    // );
-
-    socket.on('update', 
-        function(data) {
-            //console.log("new Pos: " + socket.id + " x: " + data.x + " y: " + data.y + " r: " + data.r);
-            
-            var player;
-            for(var i = 0; i < players.length; ++i){
-                if (socket.id == players[i].id) {
-                    player = players[i];
-                }
-            }
-
-            player.pos.x = data.x;
-            player.pos.y = data.y;
-            player.r = data.r;
-        }
-    );
-
-
-    socket.on('newbullet', 
-        function(bulletdata){
-            bullets.push(new Bullet(bullets.length+1, bulletdata.x, bulletdata.y, bulletdata.dir))
-            console.log("generated new bullet: " + bullets[bullets.length-1])
-        }
-    )
-
-    socket.on('disconnect',
-        function() {
-            console.log('Client has disconnected');
-        }
-    )
 }
+
+
+
+
+const room = new Room();
+function joinGame(username='krem'){
+    console.log('joining game: ' + this.id);
+    room.addPlayer(this, 'kremenevskiy');
+}
+
+var cnt = 0;
+function updatePlayer(data){
+    // if (cnt < 3) {
+    //     console.log('before player update:')
+    //     console.log(room.players[this.id]);
+    // }
+    const playerId = this.id;
+    room.updatePlayer(playerId, data.x, data.y, data.dir);
+    // if (cnt < 3) {
+    //     console.log('after updating')
+    //     console.log(room.players[this.id])
+    // }
+    // cnt++;
+}
+
+
+function addBullet(bulletdata){
+    room.addBullet(this.id, bulletdata.x, bulletdata.y, bulletdata.dir);
+}
+
+
+function onDisconnect(){
+    console.log("Player: " + this.id + "disconnected!");
+    room.removePlayer(this);
+}
+
+
+// var v = new Vector(2, 3);
+// class someClass {
+//     constructor(x, y){
+//         this.vec = new Vector(x, y);
+//         this.somevar = 'krem'
+//     }
+// }
+//
+//
+// var some = new someClass(100, 200);
+
+//
+// console.log('vector');
+// console.log(v)
+// console.log('someclass');
+// console.log(some);
 
