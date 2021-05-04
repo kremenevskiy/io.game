540219b9ca14d8de607272dfd42381fdacea3aa2
kremenevskiy <kremeneuski@gmail.com>
2021-05-05 02:47:06 +0300

    constants impovements balance

--- constants.js
+++ constants.js
@@ -4,14 +4,18 @@ module.exports = Object.freeze({
     PLAYERS_SPEED: 100,
 
     BULLET_RADIUS: 3,
-    BULLET_SPEED: 300,
+    BULLET_SPEED: 50,
     BULLET_DAMAGE: 10,
 
     FOOD_RADIUS: 4,
 
     MAP_SIZE: 3000,
     MSG_TYPES: {
+
+        UPDATE_INPUT: 'update_input',
+
         JOIN_GAME: 'join_game',
+        GAME_UPDATE: 'game_update',
         GAME_OVER: 'dead'
     }
 });
