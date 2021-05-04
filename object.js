29f16362b5e4c88b8f26e050eac874c6de588205
kremenevskiy <kremeneuski@gmail.com>
2021-05-05 02:46:19 +0300

    fixed serialization

--- object.js
+++ object.js
@@ -1,18 +1,32 @@
 const Vector = require('./vector')
+const Constants = require('./constants')
 
 class Object {
     constructor(id, x, y){
         this.id = id;
         this.pos = new Vector(x, y);
+        this.x = x;
+        this.y = y;
     }
 
+    checkMapConstraints() {
+        return !!((Math.abs(this.pos.x) <= Constants.MAP_SIZE) && (Math.abs(this.pos.y) <= Constants.MAP_SIZE))
+    }
+
+
     serializeForUpdate(){
+        // console.log("Coordinates: X: " + this.pos + "Y: " + this.pos);
+        // console.log(this.pos);
         return {
             id: this.id,
             position: {
                 x: this.pos.x,
                 y: this.pos.y
-            }
+            },
+            // x: this.pos.x,
+            // y: this.pos.y,
+            // xx: this.x,
+            // yy: this.y
         };
     }
 }
