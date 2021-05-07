1e2bbad7256c15ec2e46f9213615e3e965c84bf9
kremenevskiy <kremeneuski@gmail.com>
2021-05-07 03:37:25 +0300

    map contrained update

--- src/server/player.js
+++ src/server/player.js
@@ -27,13 +27,14 @@ class Player extends MovableObject {
         // console.log("vel to update: x:" + vel.x + " y: " + vel.y);
         vel.div(10);
         vel.limit(4);
-        console.log("\t\t\tvelocity new  to update: x:" + this.velocity.x + " y: " + this.velocity.y);
+        // console.log("\t\t\tvelocity new  to update: x:" + this.velocity.x + " y: " + this.velocity.y);
 
         this.velocity.lerp(vel, 0.1);
-        console.log("\t\t\tvelocity lerp:: x:" + this.velocity.x + " y: " + this.velocity.y);
+        // console.log("\t\t\tvelocity lerp:: x:" + this.velocity.x + " y: " + this.velocity.y);
 
         this.pos.add(this.velocity);
-        console.log("to x: " + this.pos.x + "y: " + this.pos.y)
+        this.constrain(-Constants.MAP_SIZE, Constants.MAP_SIZE);
+        // console.log("to x: " + this.pos.x + "y: " + this.pos.y)
 
     }
 
