470459302921c23a89c3d227078330264c6f94cb
kremenevskiy <kremeneuski@gmail.com>
2021-05-08 00:46:07 +0300

    bullet new propetries: damage

--- src/server/bullet.js
+++ src/server/bullet.js
@@ -3,10 +3,12 @@ const Constants = require('../shared/constants')
 
 
 class Bullet extends MovableObject {
-    constructor(parentID, x, y, dir, speed = Constants.BULLET_SPEED){
+    constructor(parentID, x, y, dir, speed = Constants.BULLET_SPEED, damage = Constants.BULLET_DAMAGE,
+                radius = Constants.BULLET_RADIUS){
         super(parentID, x, y, dir, speed);
+        this.r = radius;
         this.parentID = parentID;
-
+        this.damage = damage;
     }
 }
 
