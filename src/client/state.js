8e0da613461f7c5c324e1bdfc588bebed8574a37
kremenevskiy <kremeneuski@gmail.com>
2021-05-12 16:27:03 +0300

    disable upgrade menu when no update points

--- src/client/state.js
+++ src/client/state.js
@@ -1,14 +1,24 @@
-    import {updateLeaderboard} from "./leaderboard";
+import {updateLeaderboard} from "./leaderboard";
+
+const upgradeMenu = document.getElementById('upgrade-menu');
+
 
 const RENDER_DELAY = 100;
 
 const gameUpdates = [];
 var lastGameUpdate = null;
 var got_update = false;
+
 export function processGameUpdate(update) {
     got_update = true;
     lastGameUpdate = update;
     updateLeaderboard(update.leaderboard)
+    if (update.me.update_points < 1) {
+        upgradeMenu.classList.add('disabled');
+    }
+    else {
+        upgradeMenu.classList.remove('disabled');
+    }
     // console.log('got new update from server')
     // console.log(update)
 }
